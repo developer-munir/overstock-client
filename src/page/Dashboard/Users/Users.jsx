c4dbfc72b2c09d2edc7f1a03f3a46e81f03a0637
developer-munir <devolopermunir@gmail.com>
2022-11-26 20:52:13 +0600

    make admin successfull

--- src/page/Dashboard/Users/Users.jsx
+++ src/page/Dashboard/Users/Users.jsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import toast from "react-hot-toast";
 import PrimaryBtn from "../../Shared/Buttons/PrimaryBtn";
 
 const Users = () => {
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: [],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/users");
@@ -11,6 +12,19 @@ const Users = () => {
       return data;
     },
   });
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          toast.success("Got adminship successfully");
+          refetch();
+        }
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div className="overflow-x-auto w-full">
       <table className="table w-full">
@@ -23,7 +37,7 @@ const Users = () => {
         </thead>
         <tbody>
           {data?.map((user) => (
-            <tr className="border">
+            <tr className="border" key={user?._id}>
               <td className="border">
                 <div className="flex items-center space-x-3">
                   <div className="avatar">
@@ -44,8 +58,19 @@ const Users = () => {
                   Desktop Support Technician
                 </span>
               </td>
-                  <td className="border">
-                      <PrimaryBtn>Make Admin</PrimaryBtn>
+              <td className="border">
+                {user?.role === "admin" ? (
+                  <button className="hover:text-[#cad5e2] bg-[#cad5e2] text-[#03203C] py-3 px-6 font-semibold hover:bg-red-400">
+                    Remove Admin{" "}
+                  </button>
+                ) : (
+                  <button
+                    className="hover:text-[#cad5e2] bg-[#cad5e2] text-[#03203C] py-3 px-6 font-semibold hover:bg-red-400"
+                    onClick={() => handleMakeAdmin(user?._id)}
+                  >
+                    Make Admin
+                  </button>
+                )}
               </td>
             </tr>
           ))}
